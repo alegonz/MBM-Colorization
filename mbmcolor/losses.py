Reformulated the loss function as logsumexp for numerical stability. Fixed bug in activation function of mu layer.

mbmcolor/losses.py
@@ -41,15 +41,15 @@ def build_mbm_log_likelihood(input_shape, m):
         # Get MBM parameters
         # Parameters are concatenated along the second axis
         # tf.split expect sizes, not locations
-        prior, mu = K.tf.split(y_pred, num_or_size_splits=splits, axis=1)
+        log_prior, mu = K.tf.split(y_pred, num_or_size_splits=splits, axis=1)
 
-        y_true = K.expand_dims(y_true, axis=2)
+        y_true = K.tile(K.expand_dims(y_true, axis=2), [1, 1, m])
         mu = K.reshape(mu, [-1, height*width*2, m])  # -1 is for the sample dimension
-        prob = K.pow(mu, y_true) * K.pow(1 - mu, 1 - y_true)
-        prob = K.prod(prob, axis=1)
+        prob = K.binary_crossentropy(y_true, mu, from_logits=False)
+        prob = K.sum(prob, axis=1)
 
-        prob_mixture = K.sum(prior * prob, axis=1)
+        exponent = log_prior + prob
 
-        return -K.log(prob_mixture)
+        return -K.logsumexp(exponent, axis=1)
 
     return _mbm_log_likelihood

mbmcolor/model.py
@@ -7,6 +7,7 @@ from keras.models import Model, load_model
 from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger, TensorBoard, TerminateOnNaN
 
 from mbmcolor.losses import build_mbm_log_likelihood
+from mbmcolor.activations import log_softmax
 
 
 class MBMColorNet(object):
@@ -109,17 +110,17 @@ class MBMColorNet(object):
         # ------ Output layers that parametrize a Multivariate Bernoulli Mixture Density.
         # Means
         # There are n_components for each of the 2 color channels.
-        mu = Conv2D(self.n_components * 2, kernel_size=self.kernel_size, padding='same')(layer)
+        mu = Conv2D(self.n_components * 2, kernel_size=self.kernel_size, padding='same', activation='sigmoid')(layer)
         mu = Flatten()(mu)
 
         # Priors
         # First squeeze the filters with a convolution before flattening
-        prior = Conv2D(1, kernel_size=self.kernel_size, padding='same')(bottleneck)
-        prior = Flatten()(prior)
-        prior = Dense(self.n_dense_prior, activation='relu')(prior)
-        prior = Dense(self.n_components, activation='softmax')(prior)
+        log_prior = Conv2D(1, kernel_size=self.kernel_size, padding='same')(bottleneck)
+        log_prior = Flatten()(log_prior)
+        log_prior = Dense(self.n_dense_prior, activation='relu')(log_prior)
+        log_prior = Dense(self.n_components, activation=log_softmax)(log_prior)
 
-        self._output_layer = Concatenate(axis=-1)([prior, mu])
+        self._output_layer = Concatenate(axis=-1)([log_prior, mu])
 
     def _compile_model(self):
         self._model.compile(optimizer=self.optimizer, loss=self._loss)
@@ -201,11 +202,11 @@ class MBMColorNet(object):
 
         # Get MBM parameters
         # Parameters are concatenated along the second axis
-        prior, mu = np.split(pred, axis=1, indices_or_sections=splits)
+        log_prior, mu = np.split(pred, axis=1, indices_or_sections=splits)
 
         mu = np.reshape(mu, (-1, height, width, m))
 
-        which = prior.argmax(axis=1)
+        which = log_prior.argmax(axis=1)
         sample, height, width = np.indices(array.shape[:-1])
 
         return np.expand_dims(mu[sample, height, width, which], axis=3)